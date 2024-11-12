d61d45695aca5a6c62212c758caa66ad7bb4d2d1
antony <antoloo254@gmail.com>
2024-11-12 06:15:46 +0300

    backend created

--- coupon-discount-app/src/App.js
+++ coupon-discount-app/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CouponList from './components/CouponList';
+import CouponForm from './components/CouponForm';
+import CouponDetail from './components/CouponDetail';
+import About from './components/About';
 
 function App() {
   return (
-    <div> 
-      Coupon Discount  App
-    
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={CouponList} />
+          <Route path="/add" component={CouponForm} />
+          <Route path="/coupons/:id" component={CouponDetail} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
